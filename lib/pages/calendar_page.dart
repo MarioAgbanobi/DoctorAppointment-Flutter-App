e06ae64e22d0575744560324a79ae99e6777ea99
stanley <stanleyagbanobi@gmail.com>
2025-08-23 14:13:54 +0100

    Updated homepage, added chats and profile screen & Updated calendar page

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,12 +1,44 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: const Row(
+          children: [
+            Text("Your Schedule")
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications_none),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(10),
+        children: [
+          // Segmented controllers 
+          CupertinoSlidingSegmentedControl(
+            padding: const EdgeInsets.all(16),
+            children: const {
+            0: Text("Upcoming"),
+            1: Text("Complete"),
+            2: Text("Results")},
+            onValueChanged: (value) {
+
+            })
+        ],
+      ),
     );
   }
 }
