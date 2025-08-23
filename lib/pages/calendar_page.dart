0864f0fa29d2dfcdd8a3f255314069386c19f696
stanley <stanleyagbanobi@gmail.com>
2025-08-23 14:22:03 +0100

    Created a segemented control

--- lib/pages/calendar_page.dart
+++ lib/pages/calendar_page.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   const CalendarPage({super.key});
+
+  @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  int activeIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -30,12 +37,15 @@ class CalendarPage extends StatelessWidget {
           // Segmented controllers 
           CupertinoSlidingSegmentedControl(
             padding: const EdgeInsets.all(16),
+            groupValue: activeIndex,
             children: const {
             0: Text("Upcoming"),
             1: Text("Complete"),
             2: Text("Results")},
             onValueChanged: (value) {
-
+              setState(() {
+                activeIndex = value!;
+              });
             })
         ],
       ),
